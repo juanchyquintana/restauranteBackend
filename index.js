a91c06ed79c8a1557697d70fa84f7d6f93db1c34
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-03-28 20:06:42 -0300

    rutas agregadas para los productos, usuarios y pedidos

--- index.js
+++ index.js
@@ -4,18 +4,15 @@ import "dotenv/config"
 import morgan from "morgan"
 import { fileURLToPath } from "url"
 import path from 'path'
-// import xRouter from "./src/routes/colores.routes.js"
 import "./src/database/database.js"
+import restauranteRouter from './src/routes/restaurante.routes.js'
 
 const app = express()
-
-// app.set('port', process.env.PORT || 4030)
 app.set('port', 4030)
 
 app.listen(app.get('port'),()=>{
     console.log('Estoy vivo ')
     console.log(`Estoy en el puerto ${app.get('port')}`)
-    
 })
 
 app.use(cors())
@@ -31,5 +28,4 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 app.use(express.static(path.join(__dirname, '/public')))
-
-// app.use('/api', xRouter)
+app.use("/api", restauranteRouter)
