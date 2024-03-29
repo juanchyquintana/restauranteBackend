54cb6eae120f83608d74edc2de2d69e5827f25d4
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-03-29 14:49:59 -0300

    se creó el modelo de usuarios para la base de datos. Se pide nombre, email, password, estado y tipo de usuario. También se hasheo el password

---
+++ src/database/models/Usuario.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const usuarioSchema = new mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 3,
+    maxLength: 100
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    required: true,
+    unique: true,
+    trim: true,
+    validate: {
+        validator: (value) => {
+            const pattern = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i
+            return pattern.test(value)
+        }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  estado: {
+    type: String,
+    enum: ["activo", "inactivo"],
+    default: "Activo",
+  },
+  tipoUsuario: {
+    type: String,
+    enum: ["admin", "usuario"],
+    default: "usuario",
+  },
+});
+
+usuarioSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+  next();
+});
+
+const Usuario = mongoose.model("Usuarios", usuarioSchema);
+export default Usuario;
