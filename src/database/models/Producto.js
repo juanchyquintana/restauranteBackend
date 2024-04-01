70fe6260ea1590c691a1ef8eac7babf197726a1e
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-04-01 00:25:44 -0300

    cree el Schema de producto, con las propiedades: nombre, estado, precio, detalle, categoria e imagen

---
+++ src/database/models/Producto.js
@@ -0,0 +1,46 @@
+import mongoose, {Schema} from "mongoose";
+
+const productoSchema = new Schema({
+    nombre: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 2,
+        maxLength: 50
+    },
+    estado: {
+        type: String,
+        required: true,
+        trim: true,
+        enum: ["disponible", "No disponible"]
+    },
+    precio: {
+        type: Number,
+        required: true,
+        min: 50,
+        max: 10000
+    },
+    detalle: {
+        type: String, 
+        required: true,
+        trim: true,
+        minLength: 50,
+        maxLength: 300
+    },
+    categoria: {
+        type: String,
+        required: true,
+        enum: [
+            "Entradas",
+            "Platos Principales",
+            "Postres",
+            "Bebidas",
+          ]
+    },
+    imagen: {
+        type: String,
+        required: true
+    }
+})
+
+const Producto = mongoose.model("producto", productoSchema)
