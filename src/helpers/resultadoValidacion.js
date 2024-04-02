7241194d33b3937df76756f47661b8320e435a28
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-04-02 14:34:40 -0300

    corregi un error de resultadoValidacion, falta que isEmpty sea un metodo

--- src/helpers/resultadoValidacion.js
+++ src/helpers/resultadoValidacion.js
@@ -2,7 +2,7 @@ import { validationResult } from "express-validator";
 
 const resultadoValidacion = (req, res, next) => {
     const errors = validationResult(req)
-    if (!errors.isEmpty){
+    if (!errors.isEmpty()){
         return res.status(400).json({errores: errors.array()})
     }
     next()
