a3b7f158450af29c48204cd3dab0258e6aa4e933
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-04-02 12:24:25 -0300

    controlador de usuario creado en usuario.controller

---
+++ src/controllers/usuarioController.js
@@ -0,0 +1,20 @@
+import Usuario from "../database/models/Usuario";
+
+const editarUsuario = async (req, res) => {
+  try {
+    const buscarProducto = await Usuario.findById(req.params.id);
+    if (!buscarProducto) {
+      return res
+        .status(404)
+        .json({ mensaje: "No se pudo editar el usuario, el id es incorrecto" });
+    }
+
+    await Usuario.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).json({ mensaje: "El usuario fue modificado exitosamente" });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrio un error al intentar editar un usuario" });
+  }
+};
