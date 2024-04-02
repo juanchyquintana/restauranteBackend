5047931918efaa5e6ab463755f9cfc402fab9921
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-04-02 13:44:25 -0300

    se agregó el controllador get de los pedidos. Se agregó un populate para incluir informacion del usuario y los productos agregados

--- src/routes/restaurante.routes.js
+++ src/routes/restaurante.routes.js
@@ -1,9 +1,13 @@
 import {Router} from 'express'
+import { obtenerPedidos } from '../controllers/pedidosController.js';
 
 const router = Router();
 
 router.route("/usuarios")
 router.route("/productos")
+
+
 router.route("/pedidos")
+    .get(obtenerPedidos)
 
 export default router
