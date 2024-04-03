16c0d5657031de4a33749c4476d9ec2d366f711b
K Stephan Torres <kevinstephantorres@gmail.com>
2024-04-03 18:42:22 -0300

    Se cambio el nombre de una variable para que concuerde con su funcionalidad

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -2,8 +2,8 @@ import Usuario from "../database/models/Usuario.js";
 
 export const editarUsuario = async (req, res) => {
   try {
-    const buscarProducto = await Usuario.findById(req.params.id);
-    if (!buscarProducto) {
+    const buscarUsuario = await Usuario.findById(req.params.id);
+    if (!buscarUsuario) {
       return res
         .status(404)
         .json({ mensaje: "No se pudo editar el usuario, el id es incorrecto" });
