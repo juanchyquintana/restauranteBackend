a95a219033e5f2964fd0facd16dc4e0e319ee9a5
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-04-03 11:42:21 -0300

    se creó el controlador para editar un pedido

--- src/controllers/pedidosController.js
+++ src/controllers/pedidosController.js
@@ -10,7 +10,20 @@ const obtenerPedidos = async (req, res) => {
     }
 }
 
-const editarPedido = async (req, res) => {}
+const editarPedido = async (req, res) => {
+    try {
+        const buscarPedido = await Pedidos.findById(req.params.id)
+        if(!buscarPedido) {
+            return res.status(404).json({ mensaje: 'Producto No Encontrado'})
+        }
+
+        await Pedidos.findByIdAndUpdate(req.params.id, req.body)
+        res.status(200).json({ mensaje: "Producto Editado Correctamente!"})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ mensaje: 'Error al Editar el Pedido' })
+    }
+}
 
 const validarPedidos = async (req, res, next) => {}
 
