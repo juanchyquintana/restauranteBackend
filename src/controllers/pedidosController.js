1bc0be1a0b8dacdb2b2cf0f161a5e1fa7d351a90
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-04-08 16:31:57 -0300

    cree el controlador de crear pedido e hice algunas correcciones con el nombre del archivo pedido.js. Tambien corregi las importanciones del archivo y corregi los usos del objeto dentro del mismo controlador.

--- src/controllers/pedidosController.js
+++ src/controllers/pedidosController.js
@@ -1,32 +1,44 @@
-import Pedidos from '../database/models/Pedidos.js';
+import Pedido from '../database/models/Pedido.js';
+
+const crearPedido = async (req, res) => {
+    try {
+        const nuevoPedido = new Pedido(req.body)
+        await nuevoPedido.save()
+        res.status(201).json({mensaje: "El pedido fue creado correctamente."})
+    } catch (error) {
+        console.log(error)
+        res.satus(400).json({mensaje: "No se pudo crear el pedido."})
+    }
+}
 
 const obtenerPedidos = async (req, res) => {
     try {
-        const pedidos = await Pedidos.find().populate('usuario').populate('productos.producto')
+        const pedidos = await Pedido.find().populate('usuario').populate('productos.producto')
         res.json(pedidos)
     } catch (error) {
         console.log(error)
-        res.status(500).json({ mensaje: 'Hubo un error al obtener los pedidos'})
+        res.status(500).json({ mensaje: 'Hubo un error al obtener los pedidos.'})
     }
 }
 
 const editarPedido = async (req, res) => {
     try {
-        const buscarPedido = await Pedidos.findById(req.params.id)
+        const buscarPedido = await Pedido.findById(req.params.id)
         if(!buscarPedido) {
-            return res.status(404).json({ mensaje: 'Producto No Encontrado'})
+            return res.status(404).json({ mensaje: 'Producto No Encontrado.'})
         }
 
-        await Pedidos.findByIdAndUpdate(req.params.id, req.body)
-        res.status(200).json({ mensaje: "Producto Editado Correctamente!"})
+        await Pedido.findByIdAndUpdate(req.params.id, req.body)
+        res.status(200).json({ mensaje: "Producto Editado Correctamente!."})
     } catch (error) {
         console.log(error)
-        res.status(500).json({ mensaje: 'Error al Editar el Pedido' })
+        res.status(500).json({ mensaje: 'Error al Editar el Pedido.' })
     }
 }
 
 
 export {
     obtenerPedidos,
-    editarPedido
+    editarPedido,
+    crearPedido
 }
