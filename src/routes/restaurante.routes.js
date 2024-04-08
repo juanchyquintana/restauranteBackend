d1eaa29bfbbbdcdcbbd6c541ecd16a08bd207b37
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-04-08 12:02:30 -0300

    se agregó la ruta para editar

--- src/routes/restaurante.routes.js
+++ src/routes/restaurante.routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { editarUsuario, obtenerUsuario, crearUsuario,  borrarUsuario, verUsuarios } from '../controllers/usuarioController.js';
+import { editarProducto } from '../controllers/productoController.js';
 import { obtenerPedidos, editarPedido } from '../controllers/pedidosController.js';
 import validacionesUsuario from '../helpers/validacionUsuario.js';
 import validacionPedidos from '../helpers/validacionPedidos.js';
@@ -8,11 +9,12 @@ const router = Router();
 
 router.route("/usuarios").get(verUsuarios).post(validacionesUsuario, crearUsuario)
 router.route("/usuarios/:id").put([validacionesUsuario], editarUsuario).get(obtenerUsuario).delete(borrarUsuario)
+
 router.route("/productos")
+router.route("/productos/:id").put(editarProducto)
 
 router.route("/pedidos")
     .get(obtenerPedidos)
-
 router.route("/pedidos/:id")
     .put([validacionPedidos], editarPedido)
 
