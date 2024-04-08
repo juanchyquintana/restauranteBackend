e5f5f4ce186aa42028e181c61c00f72c64581d07
K Stephan Torres <kevinstephantorres@gmail.com>
2024-04-08 20:08:45 -0300

    Se formateo el codigo

--- src/routes/restaurante.routes.js
+++ src/routes/restaurante.routes.js
@@ -11,7 +11,7 @@ const router = Router();
 router.route("/usuarios").get(verUsuarios).post(validacionesUsuario, crearUsuario)
 router.route("/usuarios/:id").put([validacionesUsuario], editarUsuario).get(obtenerUsuario).delete(borrarUsuario)
 
-router.route("/productos").get(listarProductos).post([validacionProducto], crearProducto)
+router.route("/productos").get(listarProductos).post(validacionProducto, crearProducto)
 router.route("/productos/:id").put([validacionProducto] ,editarProducto).get(obtenerProducto)
 
 router.route("/pedidos").post([validacionPedidos], crearPedido)
