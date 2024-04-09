822ca3ca426e7d441d4fde1bf5f4124a98afe513
Nano <luxorius07@gmail.com>
2024-04-08 22:14:43 -0300

    se agrego el GET a router de pedidos

--- src/routes/restaurante.routes.js
+++ src/routes/restaurante.routes.js
@@ -1,22 +1,48 @@
-import { Router } from 'express'
-import { editarUsuario, obtenerUsuario, crearUsuario,  borrarUsuario, verUsuarios } from '../controllers/usuarioController.js';
-import { editarProducto, obtenerProducto, listarProductos } from '../controllers/productoController.js';
-import { obtenerPedidos, editarPedido, crearPedido } from '../controllers/pedidosController.js';
-import validacionesUsuario from '../helpers/validacionUsuario.js';
-import validacionProducto from '../helpers/validacionProducto.js';
-import validacionPedidos from '../helpers/validacionPedidos.js';
+import { Router } from "express";
+import {
+  editarUsuario,
+  obtenerUsuario,
+  crearUsuario,
+  borrarUsuario,
+  verUsuarios,
+} from "../controllers/usuarioController.js";
+import {
+  editarProducto,
+  obtenerProducto,
+  listarProductos,
+} from "../controllers/productoController.js";
+import {
+  obtenerPedidos,
+  editarPedido,
+  crearPedido,
+  obtenerPedidoPorId,
+} from "../controllers/pedidosController.js";
+import validacionesUsuario from "../helpers/validacionUsuario.js";
+import validacionProducto from "../helpers/validacionProducto.js";
+import validacionPedidos from "../helpers/validacionPedidos.js";
 
 const router = Router();
 
-router.route("/usuarios").get(verUsuarios).post(validacionesUsuario, crearUsuario)
-router.route("/usuarios/:id").put([validacionesUsuario], editarUsuario).get(obtenerUsuario).delete(borrarUsuario)
+router
+  .route("/usuarios")
+  .get(verUsuarios)
+  .post(validacionesUsuario, crearUsuario);
+router
+  .route("/usuarios/:id")
+  .put([validacionesUsuario], editarUsuario)
+  .get(obtenerUsuario)
+  .delete(borrarUsuario);
 
-router.route("/productos").get(listarProductos)
-router.route("/productos/:id").put([validacionProducto] ,editarProducto).get(obtenerProducto)
+router.route("/productos").get(listarProductos);
+router
+  .route("/productos/:id")
+  .put([validacionProducto], editarProducto)
+  .get(obtenerProducto);
 
-router.route("/pedidos").post([validacionPedidos], crearPedido)
-    .get(obtenerPedidos)
-router.route("/pedidos/:id")
-    .put([validacionPedidos], editarPedido)
+router
+  .route("/pedidos")
+  .post([validacionPedidos], crearPedido)
+  .get(obtenerPedidos);
+router.route("/pedidos/:id").put([validacionPedidos], editarPedido).get(obtenerPedidoPorId);
 
-export default router
+export default router;
