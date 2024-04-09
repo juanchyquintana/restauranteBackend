5275bac32cf34f9153945927cefb5730b4201eda
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-04-09 10:41:51 -0300

    se modificó el archivo principal para poder recibir el puerto del back una vez deployado

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
-import express from "express";
-import cors from "cors";
-import "dotenv/config";
-import morgan from "morgan";
-import { fileURLToPath } from "url";
-import path from "path";
-import "./src/database/database.js";
 import restauranteRouter from "./src/routes/restaurante.routes.js";
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import "./src/database/database.js";
+import "dotenv/config";
 
 const app = express();
-app.set("port", 4030);
 
+app.set("port", process.env.PORT || 4030);
 app.listen(app.get("port"), () => {
-  console.log("Estoy vivo ");
-  console.log(`Estoy en el puerto ${app.get("port")}`);
+  console.log("Estoy vivo");
+  console.log(`Estoy en el puerto - ${app.get("port")}`);
 });
 
 app.use(cors());
-
 app.use(morgan("dev"));
-
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
 const __filename = fileURLToPath(import.meta.url);
-
 const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "/public")));
-
 app.use("/api", restauranteRouter);
